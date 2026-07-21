55a0b4ab6936d90b350b041108ae8eae27ea2818
ssoyeeon <ssuu2039@gmail.com>
2026-07-21 10:36:10 +0900

    [Feat] 바다까지 끝

--- Wep_Game/Assets/Scripts/BallSpeedGame/WallDestroyer.cs
+++ Wep_Game/Assets/Scripts/BallSpeedGame/WallDestroyer.cs
@@ -2,11 +2,20 @@
 
 public class WallDestroyer : MonoBehaviour
 {
+    public GameObject DoneCanvas;
+    private void Awake()
+    {
+        DoneCanvas.SetActive(false);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Map"))
         {
             Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            DoneCanvas.SetActive(true);
         }
     }
 }
